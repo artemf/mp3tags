48f6ab1e7508f4454663cb8c19e0dbba5b3ac7e8
Artem Fedorov <artemf@mail.ru>
2014-06-21 11:22:46 +0400

    Code cleanup, more compatible

--- app/controllers/audio_files_controller.rb
+++ app/controllers/audio_files_controller.rb
@@ -14,6 +14,33 @@ class AudioFilesController < ApplicationController
   end
 
   def show
+    respond_to do |format|
+      format.html do
+        if @audio_file.processing
+          render action: 'processing'
+        elsif @audio_file.info
+          @info = JSON.parse(@audio_file.info)
+          @tags = @info.dup
+          @tags.delete('pictures')
+          @tags2 = @info['tags2'].dup
+          @pictures = @info['pictures'] || []
+          @info['tags2'].keys.each do |key|
+            if ID3v2::TAGS.has_key? key
+              name = ID3v2::TAGS[key]
+              value = @info['tags2'][key]
+              @info['tags2'].delete(key)
+              @info['tags2'][name] = value
+            end
+          end
+          render action: 'info'
+        elsif @audio_file.error
+          render action: 'error'
+        else
+          redirect_to root_path
+        end
+      end
+      format.json { render action: 'processing' }
+    end
   end
 
   private
